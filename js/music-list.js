788e3323bec36849c41aa840de63de5a87d8f45d
edenjoyer <ediheryan@gmail.com>
2023-03-03 07:58:09 +0700

    deleted shit

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+//playlist songs//
+let allMusic = [
+    {
+        name: "Tokyo Music Walker, Takahiro N - Morning Station",
+        artist: "Tokyo Music Walker, Takahiro N",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "The XX - Intro",
+        artist: "The XX",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "ATCH - Timeout",
+        artist: "ATCH",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Hotham - Alone",
+        artist: "Hotham",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Onycs - Shine",
+        artist: "onycs",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Roa - Remember (JayJen Remix)",
+        artist: "Roa",
+        img: "music-6",
+        src: "music-6"
+    },
+];
