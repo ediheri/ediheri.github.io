a9a5cb0b110a259ebebee85c5724a9ca5304274e
edenjoyer <ediheryan@gmail.com>
2023-03-04 11:20:59 +0700

    connect to php

---
+++ js/form.js
@@ -0,0 +1,30 @@
+const form = document.querySelector("form"),
+statusTxt = form.querySelector(".button-area span");
+
+form.onsubmit = (e)=>{
+    e.preventDefault();
+    statusTxt.style.color = "#0D6EFD";
+    statusTxt.style.display = "block";
+
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "message.php", true);
+    xhr.onload = ()=>{
+        if(xhr.readyState == 4 && xhr.status == 200){
+            let response = xhr.response;
+            if(response.indexOf("email and password field is required!")!= -1 || response.indexOf("emter a valid email address") || response.indexOf("Sorry, failed to send your message!")){
+                statusTxt.style.color = "red";
+            }else{
+                form.reset();
+                setTimeout(()=>{
+                    statusTxt.style.display = "name";
+                }, 3000);
+            }
+            
+
+            statusTxt.innerText = response;
+        }
+    }
+    let formData = new FormData(form);
+    xhr.send(formData);
+}
